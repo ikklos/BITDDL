788c2c6a461e94420caf78767df7efb42ae8d3bd
BottleOfErie <bottle_of_erie@outlook.com>
2023-08-23 18:03:43 +0800

    move keyboard to other file

--- behavejs/gamewindow.js
+++ behavejs/gamewindow.js
@@ -1,4 +1,5 @@
 import { HitTest, CrossTheBoader } from './collision.js'
+import { keyboard } from './keyboard.js';
 //创建app对象，把预览加入DOM,app对象建议开全局
 let app = new PIXI.Application({ width: 1000, height: 600, antialias: true });
 //neko sprite1
@@ -13,53 +14,6 @@ document.getElementById("GameWindow").appendChild(app.view);
 app.stage.addChild(neko);
 //box sprite2
 
-//键盘监听
-function keyboard(value) {
-    let key = {};
-    key.value = value;
-    key.isDown = false;
-    key.isUp = true;
-    key.press = undefined;
-    key.release = undefined;
-    //The `downHandler`
-    key.downHandler = event => {
-        if (event.key === key.value) {
-            if (key.isUp && key.press) key.press();
-            key.isDown = true;
-            key.isUp = false;
-            event.preventDefault();
-        }
-    };
-
-    //The `upHandler`
-    key.upHandler = event => {
-        if (event.key === key.value) {
-            if (key.isDown && key.release) key.release();
-            key.isDown = false;
-            key.isUp = true;
-            event.preventDefault();
-        }
-    };
-
-    //Attach event listeners
-    const downListener = key.downHandler.bind(key);
-    const upListener = key.upHandler.bind(key);
-
-    window.addEventListener(
-        "keydown", downListener, false
-    );
-    window.addEventListener(
-        "keyup", upListener, false
-    );
-
-    // Detach event listeners
-    key.unsubscribe = () => {
-        window.removeEventListener("keydown", downListener);
-        window.removeEventListener("keyup", upListener);
-    };
-
-    return key;
-}
 //
 let left = keyboard("ArrowLeft"),
     up = keyboard("ArrowUp"),
