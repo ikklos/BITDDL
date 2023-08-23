7dd1d29c04fc7a6085431361355a16258be137c2
sayazaaa <772683180ao@gmail.com>
2023-08-23 23:46:46 +0800

    add bgm

--- behavejs/indexpage.js
+++ behavejs/indexpage.js
@@ -2,3 +2,27 @@ function startgame() {
     document.getElementById("startgamebutton").style.visibility = 'hidden';
     document.getElementById("maingameframe").style.visibility = 'visible';
 }
+
+//BGM播放和切换
+let bgm = document.getElementById("bgm");
+function playMusic() {
+    bgm.play();
+}
+let bgmStarted = false;
+const startPlayBGM = () => {
+    if (bgmStarted||bgm.Readys){
+        return;
+    }
+    bgm.volume = 0.2;
+    bgmStarted = true;
+    playMusic();
+    document.removeEventListener('click', startPlayBGM);
+    document.removeEventListener('keydown', startPlayBGM);
+};
+document.addEventListener('click', startPlayBGM);
+document.addEventListener('keydown', startPlayBGM);
+
+
+// const changeBGM = (num) => {
+
+// }
