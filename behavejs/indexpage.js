d3d3a82a8dfbddc16cf20e6087a917dad2e1e21c
sayazaaa <772683180ao@gmail.com>
2023-08-24 02:18:59 +0800

    更改音乐资源加载方式

--- behavejs/indexpage.js
+++ behavejs/indexpage.js
@@ -4,25 +4,50 @@ function startgame() {
 }
 
 //BGM播放和切换
-let bgm = document.getElementById("bgm");
-function playMusic() {
-    bgm.play();
-}
-let bgmStarted = false;
+bgms = [document.createElement("audio"), document.createElement("audio"), document.createElement("audio")]
+bgms[0].setAttribute("loop", "");
+bgms[1].setAttribute("loop", "");
+bgms[2].setAttribute("loop", "");
+bgms[0].setAttribute("preload", "auto");
+bgms[1].setAttribute("preload", "auto");
+bgms[2].setAttribute("preload", "auto");
+bgms[0].setAttribute("type", "audio/mpeg");
+bgms[1].setAttribute("type", "audio/mpeg");
+bgms[2].setAttribute("type", "audio/mpeg");
+bgms[0].setAttribute("src", "/BGM/駛向水平線的彼方.mp3");
+bgms[1].setAttribute("src", "/BGM/Mapleisland.mp3");
+bgms[2].setAttribute("src", "/BGM/メインテーマ.mp3");
+document.body.appendChild(bgms[0]);
+document.body.appendChild(bgms[1]);
+document.body.appendChild(bgms[2]);
+
+let bgmStarted = false, bgmNum = 0;
 const startPlayBGM = () => {
-    if (bgmStarted||bgm.Readys){
-        return;
+    if (bgmStarted){
+        return true;
     }
-    bgm.volume = 0.2;
+    bgms[0].volume = 0.2;
+    bgms[1].volume = 0.2;
+    bgms[2].volume = 0.2;
     bgmStarted = true;
-    playMusic();
+    bgms[0].play();
     document.removeEventListener('click', startPlayBGM);
     document.removeEventListener('keydown', startPlayBGM);
+    return false;
 };
-document.addEventListener('click', startPlayBGM);
-document.addEventListener('keydown', startPlayBGM);
+function changeBGM (num) {
+    bgms.forEach((audio, index) => {
+        if (num === index) { 
+            bgms[num].volume = 0.2;
+            audio.play();
+        } 
+        if (num !== index) {
+            audio.volume = 0;
+        }
+    });
+};
+
+document.body.addEventListener('click', startPlayBGM);
+document.body.addEventListener('keydown', startPlayBGM);
 
 
-// const changeBGM = (num) => {
-
-// }
