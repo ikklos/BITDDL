df1a9c91a1476b4c378558a635fcb7f620fd13f6
sayazaaa <772683180ao@gmail.com>
2023-08-26 04:47:17 +0800

    优化css逻辑 增加统一样式表 优化存档界面ui 准备merge

--- behavejs/indexpage.js
+++ behavejs/indexpage.js
@@ -2,27 +2,30 @@
 function startgame() {
     document.getElementById("startgamebutton").style.visibility = 'hidden';
     document.getElementById("loadsavebutton").style.visibility = 'hidden';
+    document.getElementById("gametitle").style.visibility = 'hidden';
     document.getElementById("maingameframe").style.visibility = 'visible';
 }
 
 function checksaves() {
     document.getElementById("startgamebutton").style.visibility = 'hidden';
     document.getElementById("loadsavebutton").style.visibility = 'hidden';
+    document.getElementById("gametitle").style.visibility = 'hidden';
     document.getElementById("savelistframe").style.visibility = 'visible';
 }
 
 function showMainMenu() {
     document.getElementById("startgamebutton").style.visibility = 'visible';
     document.getElementById("loadsavebutton").style.visibility = 'visible';
+    document.getElementById("gametitle").style.visibility = 'visible';
     document.getElementById("maingameframe").style.visibility = 'hidden';
     document.getElementById("savelistframe").style.visibility = 'hidden';
 }
 function iframeAutoFit(iframeObj){
     setTimeout(
-        function(){if(!iframeObj) return;iframeObj.height=(iframeObj.Document?iframeObj.Document.body.scrollHeight:iframeObj.contentDocument.body.offsetHeight);}
+        function(){if(!iframeObj) return;iframeObj.height=(iframeObj.Document?iframeObj.Document.body.scrollHeight:iframeObj.contentDocument.body.offsetHeight)-50;}
     ,200)
 }
-
+//调整iframe大小
 window.onload = function () {
     iframeAutoFit(document.getElementById('maingameframe'));
     iframeAutoFit(document.getElementById('savelistframe'));
