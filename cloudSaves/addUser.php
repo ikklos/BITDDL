b1a1196eca8aebe5a50773cddf450c0f17d7a30f
sayazaaa <772683180ao@gmail.com>
2023-08-27 19:36:08 +0800

    remove some php files

--- cloudSaves/addUser.php
+++
@@ -1,7 +0,0 @@
-<?php
-$id = $_GET["userid"];
-$passwd = $_GET["userpasswd"];
-$file = fopen("passwdbase.csv", "a");
-fwrite($file, "\n" . $id . "," . $passwd);
-fclose($file);
-?>
--- cloudSaves/writeSave.php
+++
@@ -1,9 +0,0 @@
-<?php
-$id = $_GET["userid"];
-if ($id == "")
-    exit(1);
-$save = $_GET["save"];
-$file = fopen($id . ".bitddl", "w+");
-fwrite($file, $save);
-fclose($file);
-?>
