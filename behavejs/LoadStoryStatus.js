68e3614cb61b9373b2156adeb6fbd80f2559c570
ikklos <ikklos@qq.com>
2023-08-28 17:21:16 +0800

    合并

--- behavejs/LoadStoryStatus.js
+++ behavejs/LoadStoryStatus.js
@@ -1,11 +1,11 @@
 //不清楚那个存档是怎么弄的，总之这个是需要从存档里面读每一个剧情的完成情况
 
 var sto = [];
-export function LoadStories(url){
+export function LoadStories(url) {
     fetch(url)
         .then((response) => response.json())
         .then((json) => {
-            for(let i = 0; i < json.story.length; i++){
+            for (let i = 0; i < json.story.length; i++) {
                 sto[i] = json.story[i];
             }
         })
